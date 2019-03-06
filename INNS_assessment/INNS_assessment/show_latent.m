7f0edf04adf0ccf89b3ca844b71007a224c1aea6
edward_martin <32644669+em1195@users.noreply.github.com>
2019-03-06 12:51:49 +0000

    update1

---
+++ INNS_assessment/INNS_assessment/show_latent.m
@@ -0,0 +1,4 @@
+disp('latent is prinicple component variances')
+disp('they are also the eigenvalues of the covariance matrix')
+disp(' ')
+disp(latent)
---
+++ INNS_assessment/INNS_assessment/show_score.m
@@ -0,0 +1,4 @@
+disp('Scores are the representation of old data points from X, in the new PC space')
+disp('Rows = observations, Columns = components')
+disp(' ')
+disp(score)
